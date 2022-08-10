30349158ba9fafc4b3c0c6fa42b22ff12d317a92
jacksonEK <jacksonkelley98@gmail.com>
2022-08-09 20:35:20 -0500

    One unique value filter notation

--- tests/preprocessing/data/transformers/one_unique_value_filterer_datasets.py
+++ tests/preprocessing/data/transformers/one_unique_value_filterer_datasets.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 
 """
-Create datasets to test the one unique value filter
-Use variables of various types (int, string, float)
-Use datasets with missing values
+Create 4 datasets to test the one unique value filter
+Use variables of various types (int, string, float) to ensure the filter can detect them
+Use datasets with missing values to ensure they are detected by the filter
+If a column has 1 unique value and missing values, it should not be dropped
+If a column has only missing values, it should be dropped
 """
 
 int_column_with_one_uniq = pd.DataFrame(
@@ -12,21 +14,34 @@ int_column_with_one_uniq = pd.DataFrame(
           "Age": [1, 1, 22, 33, 95],
           "Country": ['Japan', 'Indonesia', 'India', 'Philippines', 'Brazil']}
 )
+"""
+The "id" column should be dropped
+"""
 
 string_column_with_one_uniq = pd.DataFrame(
     data={"id": [1, 12, 13, 31, 14],
           "Age": [1, 1, 22, 33, 95],
           "Country": ['Japan', 'Japan', 'Japan', 'Japan', 'Japan']}
 )
+"""
+The "Country" column should be dropped
+"""
 
 int_column_with_one_uniq_and_missing = pd.DataFrame(
     data={"id": [1, 12, 13, 31, 14],
           "Age": [1, 1, 1, 1, np.nan],
           "Country": ['Georgia', 'Ukraine', 'China', 'Belarus', 'Japan']}
 )
+"""
+No column should be dropped
+Make sure the "Age" column is not dropped
+"""
 
 float_column_with_one_missing = pd.DataFrame(
     data={"id": [1, 12, 13, 31, 14],
           "Age": [np.nan, np.nan, np.nan, np.nan, np.nan],
           "Country": ['Georgia', 'Ukraine', 'China', 'Belarus', 'Japan']}
 )
+"""
+The "Country" column should be dropped
+"""
--- tests/preprocessing/transformers/test_one_unique_value_filterer.py
+++ tests/preprocessing/transformers/test_one_unique_value_filterer.py
@@ -4,32 +4,11 @@ import collections
 
 class TestOneUniqueValueFilterer:
     """
-    Test the one unique value filter on various datasets to ensure it properly identifies and drops
+    Test the OneUniqueValueFilterer on various datasets to ensure it properly identifies and drops
     features with only one unique value in various circumstances
-
-    Use the datasets created by one_unique_value_filterer
-    Assert that the filter worked to check results
+    Use the datasets created in one_unique_value_filterer_datasets
+    Assert that the filter worked correctly to check results
     """
-
-    def test_int_column_with_all_uniq_result_drop_column(self):
-        df, res = self.fit_transform(datasets.int_column_with_one_uniq)
-        assert list(res.columns) == ["Age", "Country"]
-        assert len(df) == len(res)
-
-    def test_string_column_with_all_uniq_result_drop_column(self):
-        df, res = self.fit_transform(datasets.string_column_with_one_uniq)
-        assert list(res.columns) == ["id", "Age"]
-        assert len(df) == len(res)
-
-    def test_int_column_with_all_uniq_and_missing_result_not_drop_column(self):
-        df, res = self.fit_transform(datasets.int_column_with_one_uniq_and_missing)
-        assert list(res.columns) == ["id", "Age", "Country"]
-        assert len(df) == len(res)
-
-    def test_float_column_with_all_missing_result_drop_column(self):
-        df, res = self.fit_transform(datasets.float_column_with_one_missing)
-        assert collections.Counter(res.columns) == collections.Counter(["id", "Country"])
-        assert len(df) == len(res)
 
     def fit_transform(self, df):
         """
@@ -45,3 +24,59 @@ class TestOneUniqueValueFilterer:
         res = filterer.transform(df)
 
         return df, res
+
+    def test_int_column_with_all_uniq_result_drop_column(self):
+        """
+        Calls the fit_transform method on the int_column_with_one_uniq dataset
+        Checks which columns are still in the dataframe and if any rows were dropped
+        Asserts if the data was transformed correctly or not
+        If not, this function raises an assertion error
+        """
+        df, res = self.fit_transform(datasets.int_column_with_one_uniq)
+        assert list(res.columns) == ["Age", "Country"]
+        assert len(df) == len(res)
+
+    def test_string_column_with_all_uniq_result_drop_column(self):
+        """
+        Calls the fit_transform method on the string_column_with_one_uniq dataset
+        Checks which columns are still in the dataframe and if any rows were dropped
+        Asserts if the data was transformed correctly or not
+        If not, this function raises an assertion error
+        """
+        df, res = self.fit_transform(datasets.string_column_with_one_uniq)
+        assert list(res.columns) == ["id", "Age"]
+        assert len(df) == len(res)
+
+    def test_int_column_with_all_uniq_and_missing_result_not_drop_column(self):
+        """
+        Calls the fit_transform method on the int_column_with_one_uniq_and_missing dataset
+        Checks which columns are still in the dataframe and if any rows were dropped
+        Asserts if the data was transformed correctly or not
+        If not, this function raises an assertion error
+        """
+        df, res = self.fit_transform(datasets.int_column_with_one_uniq_and_missing)
+        assert list(res.columns) == ["id", "Age", "Country"]
+        assert len(df) == len(res)
+
+    def test_float_column_with_all_missing_result_drop_column(self):
+        """
+        Calls the fit_transform method on the float_column_with_one_missing dataset
+        Checks which columns are still in the dataframe and if any rows were dropped
+        Asserts if the data was transformed correctly or not
+        If not, this function raises an assertion error
+        """
+        df, res = self.fit_transform(datasets.float_column_with_one_missing)
+        assert collections.Counter(res.columns) == collections.Counter(["id", "Country"])
+        assert len(df) == len(res)
+
+"""
+Call all functions in the TestOneUniqueValueFilterer class
+If this code does not raise an assertion error then the OneUniqueValueFilterer is working correctly
+"""
+uniq = TestOneUniqueValueFilterer()
+uniq.test_int_column_with_all_uniq_result_drop_column()
+uniq.test_string_column_with_all_uniq_result_drop_column()
+uniq.test_int_column_with_all_uniq_and_missing_result_not_drop_column()
+uniq.test_float_column_with_all_missing_result_drop_column()
+
+
